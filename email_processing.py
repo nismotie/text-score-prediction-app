058701fd44bc6e108f2911c27c45fc254c1e699c
titus <tiephone@gmail.com>
2020-02-14 15:38:00 +0000

    started work on processing module

--- email_processing.py
+++ email_processing.py
@@ -1,2 +1,16 @@
 import imaplib as iml
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+class GetMail(object):
+
+    """Docstring for GetMail. """
+
+    def __init__(self):
+        self.mail_user = os.getenv("EMAIL_USERNAME")
+        self.mail_pass = os.getenv("EMAIL_USERNAME")
+
+        self.mail = iml.IMAP4_SSL("imap.google.com", port=993)
+
+        
